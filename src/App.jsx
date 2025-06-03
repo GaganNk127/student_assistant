1ca21ac4d75461d60d8cf8dfb0e6602ecde01c5b
GaganNk127 <gagannaik127@gmail.com>
2025-06-03 16:51:19 +0530

    changed name

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
                 {renderPage()}
             </main>
             <footer className="text-center py-6 text-gray-600 text-sm">
-                <p>&copy; {new Date().getFullYear()} AI Student Assistant.Built By Sumant</p>
+                <p>&copy; {new Date().getFullYear()} AI Student Assistant.Built By Gagan</p>
             </footer>
         </div>
     );
